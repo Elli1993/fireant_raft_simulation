fbff89f281a041758206016ba13b9ab94c7408ab
Elli1993 <Eli.Wittmann@gmx.de>
2018-01-15 17:22:58 +0100

    farbige ants

--- enviroment.py
+++ enviroment.py
@@ -7,14 +7,15 @@ Created on Thu Jan 11 13:18:31 2018
 import numpy as np
 import os
 import math
-from ant import ant
-#from singlestepant import ant
+# from ant import ant
+from singlestepant import ant
 import csv
-#import random !!!
+# import random !!!
 import copy
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 import imageio
+
 
 class environment(object):
     '''
@@ -25,8 +26,7 @@ class environment(object):
     self.numAnts shows the number of active ants.
     '''
 
-
-    def __init__(self, dimensions = [100, 100, 100]):
+    def __init__(self, dimensions=[100, 100, 100]):
         """
         Args:
             dimensions: int array with entries specifying the x, y and z dimensions of the env
@@ -37,19 +37,20 @@ class environment(object):
         self.numAnts = 0
         self.ants = []
         self.pictures = []
+        self.antsMoved = []
 
         return;
-    
-    def setAnts(self, number = 200, config = 'cube'):
+
+    def setAnts(self, number=200, config='cube'):
         """
         Setting the ants up in a starting configuration.
-        Args: 
+        Args:
             number: int, number of ants
             config: string, specifying the initial configuration of ants: random, circle, cube....
         """
         self.numAnts = number
 
-        if number >= (self.dimensions[0]*self.dimensions[1]*self.dimensions[2])/0.7:
+        if number >= (self.dimensions[0] * self.dimensions[1] * self.dimensions[2]) / 0.7:
             print('Too many ants for this configuration!')
 
         if (config == 'random'):
@@ -64,16 +65,16 @@ class environment(object):
         if (config == 'cube'):
             print('Setting ants up in a cube.')
             middlepoint = self.dimensions / 2
-            sidelength = np.floor(number**(1./3.))
-            startpoint = np.ndarray.astype(middlepoint - sidelength/2 , int)
+            sidelength = np.floor(number ** (1. / 3.))
+            startpoint = np.ndarray.astype(middlepoint - sidelength / 2, int)
             countx = county = 0
-            #print('Sidelength is: ',sidelength)
+            # print('Sidelength is: ',sidelength)
 
             for antindex in range(number):
                 currentant = ant(startpoint, antindex)
                 self.ants.append(copy.deepcopy(currentant))
                 self.env[tuple(startpoint)] = 2
-                #print('Ant set at ', startpoint)
+                # print('Ant set at ', startpoint)
                 if countx < sidelength:
                     # increase position in x direction
                     startpoint[0] += 1
@@ -103,11 +104,11 @@ class environment(object):
             Showing the current ant configuration in the console.
         """
         for i in range(self.dimensions[2]):
-            print(np.matrix(self.env[:,:,i]))
+            print(np.matrix(self.env[:, :, i]))
 
         return;
 
-    def showAnts(self, save = True, index = [1,1], show=False):
+    def showAnts(self, save=True, index=[1, 1], show=False):
         """
             Args:
                 save: bool, if the picture should be saved
@@ -118,34 +119,47 @@ class environment(object):
             plt.ioff()
         fig = plt.figure()
         ax = plt.axes(projection='3d')
-        plt.ylim((0,self.env.shape[1]))
-        plt.xlim((0,self.env.shape[0]))
-        ax.set_zlim(0,self.env.shape[2])
-        xdata = np.argwhere(self.env == 2)[:,0]
-        ydata = np.argwhere(self.env == 2)[:,1]
-        zdata = np.argwhere(self.env == 2)[:,2]
+        plt.ylim((0, self.env.shape[1]))
+        plt.xlim((0, self.env.shape[0]))
+        ax.set_zlim(0, self.env.shape[2])
+        xdata = np.argwhere(self.env == 2)[:, 0]
+        ydata = np.argwhere(self.env == 2)[:, 1]
+        zdata = np.argwhere(self.env == 2)[:, 2]
         plt.ylabel('y')
         plt.xlabel('x')
-        ax.scatter3D(xdata, ydata, zdata, c='b', s=500);
+        ax.scatter3D(xdata, ydata, zdata, c='b', s=100);
+        xdatajustmoved = self.ants[index[1]].position[0]
+        ydatajustmoved = self.ants[index[1]].position[1]
+        zdatajustmoved = self.ants[index[1]].position[2]
+        ax.scatter3D(xdatajustmoved, ydatajustmoved, zdatajustmoved, c='g', s=100)
+        xdataalreadymoved = []
+        ydataalreadymoved = []
+        zdataalreadymoved = []
+        for movedant in self.antsMoved:
+            if (self.env[tuple(movedant.position)] != 1):
+                xdataalreadymoved.append(movedant.position[0])
+                ydataalreadymoved.append(movedant.position[1])
+                zdataalreadymoved.append(movedant.position[2])
+        ax.scatter3D(xdataalreadymoved, ydataalreadymoved, zdataalreadymoved, c='cyan', s=100)
         xdatamove = np.argwhere(self.env == 1)[:, 0]
         ydatamove = np.argwhere(self.env == 1)[:, 1]
         zdatamove = np.argwhere(self.env == 1)[:, 2]
-        ax.scatter3D(xdatamove, ydatamove, zdatamove, c='r', s=500);
+        ax.scatter3D(xdatamove, ydatamove, zdatamove, c='r', s=100);
         if save:
             plt.savefig('ants' + str(index) + '.png')
             self.pictures.append('ants' + str(index) + '.png')
         plt.close(fig)
         return;
 
-    def performStep(self):
+    def performStep(self, releaseprob = 0.5, attachprob = 1):
         print('Performing a step!')
         oldindex = np.zeros(3)
         for antindex in self.ants:
-            #print(antindex)
+            # print(antindex)
             oldindex = np.copy(antindex.getPosition())
             self.env[tuple(antindex.getPosition())] = 0
-            antindex.checkAttach(self.env, 1)
-            #antindex.performstep(self.env, 0.5 , 1)
+            # antindex.checkAttach(self.env, 1)
+            antindex.performstep(self.env, releaseprob, attachprob)
             newindex = antindex.getPosition()
             if antindex.attached:
                 self.env[tuple(newindex)] = 2
@@ -153,27 +167,30 @@ class environment(object):
                 self.env[tuple(newindex)] = 1
             if any(newindex != oldindex):
                 print('antmoved!', oldindex, newindex)
+                self.antsMoved.append(copy.deepcopy(antindex))
         return;
 
-    def moveOneAnt(self, index = 0):
-        #print('moving ant number ', index)
+    def moveOneAnt(self, index=0):
+        # print('moving ant number ', index)
 
         oldindex = np.copy(self.ants[index].getPosition())
         self.env[tuple(self.ants[index].getPosition())] = 0
 
-        #self.ants[index].performstep(self.env, 1, 1)
-        self.ants[index].checkAttach(self.env, 1)
+        self.ants[index].performstep(self.env, 1, 1)
+        # self.ants[index].checkAttach(self.env, 1)
         newindex = self.ants[index].getPosition()
-        if self.env[tuple(newindex)] != 0:
-            print('WAAAAARNNIIINNNNGGG!!!!!')
-            print(self.env[tuple(newindex)])
-            print(self.ants[index].attached)
+        #if self.env[tuple(newindex)] != 0:
+            #print('WAAAAARNNIIINNNNGGG!!!!!')
+            #print(self.env[tuple(newindex)])
+            #print(self.ants[index].attached)
         if self.ants[index].attached:
             self.env[tuple(newindex)] = 2
         else:
             self.env[tuple(newindex)] = 1
         if any(newindex != oldindex):
             print('antmoved!', oldindex, newindex)
+            self.antsMoved.append(copy.deepcopy(self.ants[index]))
+            # print(self.antsMoved)
             return True;
         return False;
 
@@ -203,7 +220,7 @@ class environment(object):
     def saveAnts(self, filename='antconfig.csv'):
         """
         Saving ant configuration to a csv. Env shape is saved in a commented out first line.
-        Args: 
+        Args:
             filename: file handle to save ants in a file
         """
 
@@ -213,14 +230,13 @@ class environment(object):
             outfile.write('#{0}\n'.format(self.env.shape))
 
         with open(filename, 'ab') as outfile:
-
             # Iterating through a ndimensional array produces slices along
             # the last axis. This is equivalent to data[i,:,:] in this case
             for data_slice in self.env:
                 np.savetxt(outfile, data_slice, fmt='%d', delimiter=',')
 
                 # Writing out a break to indicate different slices...
-                #outfile.write('# New layer\n')
+                # outfile.write('# New layer\n')
 
         print('Data saved sucessfully to ', filename, '.')
         return;
@@ -232,7 +248,9 @@ class environment(object):
             moviename: file handle to save the gif
         """
         images = []
+        print('Creating GIF')
         for filename in self.pictures:
             images.append(imageio.imread(filename))
         imageio.mimsave(moviename, images)
+        print('GIF done')
         return;
--- main.py
+++ main.py
@@ -4,26 +4,45 @@ Created on Thu Jan 11 13:18:33 2018
 
 """
 
-from ant import ant
+#from ant import ant
 from enviroment import environment
 import numpy as np
+import csv
 
-bound = 20
-zbound = 20
-env = environment([bound,bound,zbound])
+bound = 50
+zbound = 30
+steps = 700
+env = environment([bound, bound, zbound])
+statistics = np.zeros([zbound, steps])
+#messen von zeit /steps bis sich ein layer gany auflöst - parameter ändern und das beobachten
 
-env.setAnts(50, 'cube')
-env.showAnts(save=True, index = [0,0], show=False)
+env.setAnts(300, 'cube')
+#env.showAnts(save=True, index = [0,0], show=False)
 #env.moveOneAnt(49)
-for i in range(20):
-    for index in range(50):
-        bool = env.moveOneAnt(index)
-        if bool:
-            env.showAnts(save=True, index= [i,index], show=False)
+
+for i in range(steps):
+    #for index in range(50):
+    #    bool = env.moveOneAnt(index)
+    #    if bool:
+    #        env.showAnts(save=True, index= [i,index], show=False)
+    #print('Current index ', i)
+    env.performStep(releaseprob=0.5, attachprob=0.5)
+    for layer in range(zbound):
+        if not np.all(env.env[:,:,layer]==0):
+            statistics[layer, i] = 1
+        if statistics[layer, i] != statistics[layer, i-1]:
+            print('current index: ' , i)
+            print('change in layer', layer, 'from ', statistics[layer, i-1], 'to', statistics[layer, i])
+    env.showAnts(save=True, index=[0, i], show=False)
+
+x =statistics[~(statistics==0).all(1)]
+print(x)
+np.savetxt('test.csv', x, delimiter=',', fmt='%d')
+print('datasaved')
 
 numberOfAnts = np.count_nonzero(env.env)
 print('Number of ants at end ', numberOfAnts)
-env.createGif('movie.gif')
+#env.createGif('movie.gif')
 #env.saveAnts()
 #env.loadAnts()
 
